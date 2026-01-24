7bbaa9a48992e8021e2de28d664bb6231fb4f80b
david <davidmughogho100@gmail.com>
2026-01-24 14:03:14 +0200

    added security features

--- dev/auth.py
+++ dev/auth.py
@@ -2,7 +2,7 @@ from . import db
 from .models import User
 from .forms import LoginForm, RegisterForm
 from flask_login import current_user, login_user
-from flask import Blueprint, render_template, request, url_for, redirect
+from flask import Blueprint, render_template, request, url_for, redirect, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__, template_folder="templates")
@@ -13,22 +13,28 @@ def register_page():
     if current_user.is_authenticated:
         return redirect(url_for("view.home_page"))
     form = RegisterForm()
-    if request.method == "POST":
-        full_name = request.form.get("full_name")
-        email = request.form.get("email")
-        password = request.form.get("password")
-        c_password = request.form.get("c_password")
-        h_password = generate_password_hash(password)
+    if form.validate_on_submit():
+        full_name = form.full_name.data
+        email = form.email.data
+        password = form.password.data
+        c_password = form.c_password.data
 
-        if password == c_password: 
+        # check if email already exists
+        email_check = User.query.filter_by(email=email).first()
+        if email_check:
+            flash("email already exists", category="error")
+            return redirect(url_for("auth.register_page"))
+
+        if password == c_password:
+            h_password = generate_password_hash(password)
             # enter data into the database
-        
             # step 1 create a user
             new_user = User(full_name=full_name, email=email, password=h_password)
             db.session.add(new_user)
             db.session.commit()
+            # flash suscesfull acount creation
+            flash("account sucessfully created!", category="success")
             return redirect(url_for("auth.login_page"))
-
 
     return render_template("r.html", form=form)
 
@@ -39,20 +45,22 @@ def login_page():
         return redirect(url_for("view.home_page"))
 
     form = LoginForm()
-    if request.method == "POST":
+    if form.validate_on_submit():
         # grab user data first
-        email = request.form.get("email")
-        password = request.form.get("password")
-
-        # check the database if it has this user data
+        email = form.email.data
+        password = form.password.data
+        # check if the email already exists
+       # check the database if it has this user data
         user = User.query.filter_by(email=email).first()
-
         if user and check_password_hash(user.password, password):
             # login the user and set their session
             login_user(user)
             # the user exists now we can redirect the user to the dashbord
+            flash("login sucessful!!", category="success")
             return redirect(url_for("view.home_page"))
 
+        else:
+            flash("incorect username or password", category="error")
     return render_template("l.html", form=form)
 
 
