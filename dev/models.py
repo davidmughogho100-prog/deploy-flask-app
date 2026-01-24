f9c5860ca414e76a618085a3a365490d975d450c
david <davidmughogho100@gmail.com>
2026-01-24 09:50:13 +0200

    made some few changes to the database

--- dev/models.py
+++ dev/models.py
@@ -9,7 +9,7 @@ def load_user(id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    full_name = db.Column(db.String(50))
-    email = db.Column(db.String(200), unique=True)
-    password = db.Column(db.String(200))
+    full_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
