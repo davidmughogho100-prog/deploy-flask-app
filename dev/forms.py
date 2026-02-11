b1959b500f083785f02aca184adebb4d563ee246
david <davidmughogho100@gmail.com>
2026-02-11 08:22:38 +0200

    added more security on the register page

--- dev/forms.py
+++ dev/forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Email
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Length(min=6, max=36)])
+    email = StringField("Email", validators=[DataRequired(), Email(), Length(min=6, max=36)])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=5, max=12)])
     submit = SubmitField("Login")
 
 class RegisterForm(FlaskForm):
-    full_name = StringField("Full name", validators=[DataRequired()])
+    full_name = StringField("Full name", validators=[DataRequired(), Length(min=8, max=40)])
     email = StringField("Email", validators=[DataRequired(), Length(min=6, max=36)])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=5, max=12)])
     c_password = PasswordField("confirm password", validators=[DataRequired(), Length(min=5, max=12)])
